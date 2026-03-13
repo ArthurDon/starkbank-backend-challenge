3569a36cc47fa64982a276eb34c7fc6d87785791
Arthur Donato <tucart15@gmail.com>
2026-03-13 12:57:56 -0300

    feat: limit invoice worker execution to 24 hours as required by challenge

--- workers/invoice_worker.py
+++ workers/invoice_worker.py
@@ -1,12 +1,19 @@
+from datetime import timedelta, datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from jobs.invoice_generator_job import create_invoices
 
 scheduler = BlockingScheduler()
 
-scheduler.add_job(create_invoices, "interval", hours=3)
+end_time = datetime.now() + timedelta(hours=24)
+
+scheduler.add_job(
+    create_invoices,
+    "interval",
+    hours=3,
+    end_date=end_time
+)
 
 print("Worker configurado")
-
 
 if __name__ == "__main__":
     print("Worker iniciado...")
